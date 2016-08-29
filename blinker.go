5a06de4408f8cb626111851362af1af9cbfc3394
Zack Scholl <zack.scholl@gmail.com>
2016-08-29 15:06:36 -0400

    Added blinker

---
+++ blinker.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/davecheney/gpio"
+	"github.com/davecheney/gpio/rpi"
+)
+
+func main() {
+	// set GPIO25 to output mode
+	pin, err := gpio.OpenPin(rpi.GPIO17, gpio.ModeOutput)
+	if err != nil {
+		fmt.Printf("Error opening pin! %s\n", err)
+		return
+	}
+
+	// turn the led off on exit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			fmt.Printf("\nClearing and unexporting the pin.\n")
+			pin.Clear()
+			pin.Close()
+			os.Exit(0)
+		}
+	}()
+
+	for {
+		pin.Set()
+		time.Sleep(10000 * time.Millisecond)
+		pin.Clear()
+		time.Sleep(10000 * time.Millisecond)
+	}
+}
+
