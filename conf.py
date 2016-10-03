8833cacbc1733d4aa59027bbd82f2730949446d5
Zack Scholl <zack.scholl@gmail.com>
2016-10-03 18:38:53 -0400

    Updated client

---
+++ conf.py
@@ -0,0 +1,5 @@
+# Enter the server location
+# Clone this chickencam to another computer and input the remote and filesystem
+# to store the images.
+# Make sure you send your SSH key using ssh-copy-id
+SERVER_LOCATION = "zns@cowyo.com:/www/hens/server/static/data/"
