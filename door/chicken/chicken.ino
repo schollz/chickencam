9cd03114b590d18f4e5ba51322be025287fc3e06
Zack Scholl <zack.scholl@gmail.com>
2016-10-15 16:19:15 -0400

    Updated

---
+++ door/chicken/chicken.ino
@@ -0,0 +1,55 @@
+// GET THE SUNSET AND SUNRISE TIMES FROM RUNNING
+//   go run sunset.go times
+// AND COPY AND PASTE HERE
+
+unsigned long currentTime;
+unsigned long timeTracker;
+
+void setup()
+  {
+    
+  Serial.begin (9600);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  currentTime = millis()/1000;
+  timeTracker = 0;
+  }
+  
+void loop() 
+ {
+
+  delay(1000);
+  if (millis()/1000+10000+timeTracker < currentTime) {
+    timeTracker = currentTime; // the close has reset, carry it forward
+  }
+  currentTime = millis()/1000+timeTracker;
+  int activateClose = 0;
+  for (int i=0; i < sizeof(sunset)/sizeof(sunset[0]); i++) {
+    if (currentTime > sunset[i]) {
+      activateClose = 1;
+      break;
+    }
+  }
+  if (activateClose == 1) {
+    sunsetActivation();
+  }
+
+  Serial.print(currentTime);
+  Serial.print(" sensor:");
+  int sensorValue = analogRead(1);
+  Serial.println(sensorValue);
+ }
+
+
+void sunsetActivation() {
+  Serial.println("Sunset activated!");
+  for (int i=0; i < sizeof(sunset)/sizeof(sunset[0])-1; i++) {
+    sunset[i] = sunset[i+1];
+  } 
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(30*1000);
+  
+}
